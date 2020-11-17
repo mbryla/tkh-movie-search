d7ed93495ed14e7b3c54f27775098b79d690f5c8
Mateusz Bryła <mateusz.bryla@codete.com>
2020-11-17 20:17:34 +0100

    feat: simple movie search app using omdb api

---
+++ index.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { search } from "./api";
+import { appendMovies, clearMovies, setMessage } from "./ui";
+
+(() => {
+  const handleSearchButtonClick = () => {
+    const searchTerm = document.getElementById("search-pane-input").value;
+
+    clearMovies();
+    setMessage("searching for movies, please wait…");
+
+    search(searchTerm)
+      .then((response) => {
+        if (response.Response === "True") {
+          appendMovies(response.Search);
+          setMessage();
+        } else {
+          setMessage(
+            "could not find any matches, please refine your search term"
+          );
+        }
+      })
+      .catch((error) => {
+        setMessage("unexpected error occurred, please try again later");
+      });
+  };
+
+  window.addEventListener("load", () => {
+    document
+      .getElementById("search-pane-button")
+      .addEventListener("click", handleSearchButtonClick);
+  });
+})();
